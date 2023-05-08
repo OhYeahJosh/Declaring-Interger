81cceab37927a1d7b3ddb6aaa438822f6fa62830
OhYeahJosh <130888170+OhYeahJosh@users.noreply.github.com>
2023-05-08 00:05:07 -0400

    Create Pseudo.py

---
+++ Pseudo.py
@@ -0,0 +1,13 @@
+DECLARE INTERGER
+Def Name (Ask for students name “Joshua”)
+Generate random numbers 1-500
+Number1= random range of (1,500)
+Number2= random range of (1,500)
+Ask What’s the answer to the following question!?
+Print num1 + num2
+Ask what is the sum
+Answer = input
+If answer correct print “right”
+If wrong print “wrong”
+#LOOP
+Keep generating random numbers until the user is fin.
